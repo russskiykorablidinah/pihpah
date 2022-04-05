6d91ede5193179ead452dcccdb593220612ba555
russskiykorablidinah <102823296+russskiykorablidinah@users.noreply.github.com>
2022-04-05 16:03:54 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -8,7 +8,7 @@
 ##### If output is smth like this: `Docker version 20.10.13, build a224086`, then everything is OK
 
 sudo apt-get update
-sudo apt-get install ca-certificates gnupg lsb-release
+sudo apt-get install ca-certificates gnupg lsb-release curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
