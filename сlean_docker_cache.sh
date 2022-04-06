5b29e8d43de62b0006563da3fedae871394c5599
russskiykorablidinah <102823296+russskiykorablidinah@users.noreply.github.com>
2022-04-06 14:00:28 +0300

    Update сlean_docker_cache.sh

--- сlean_docker_cache.sh
+++ сlean_docker_cache.sh
@@ -3,7 +3,7 @@
 
 
 ###### This script script was created to delete\clean all docker containters and images before reboot. Since after the reboot we will be downloading newest ones #####
-##### curl -s link | bash ####
+##### https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/%D1%81lean_docker_cache.sh | bash ####
 
 sudo docker stop $(docker ps -a -q) # Stopping all contairners which are working
 sudo docker rm $(docker ps -a -q)   # deleting them
