67e522b1f29a11b33a66d28c6be2759c25103166
russskiykorablidinah <102823296+russskiykorablidinah@users.noreply.github.com>
2022-04-11 21:04:26 +0300

    Create all.sh

---
+++ all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+curl -s https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/vnstat_install.sh | bash
+#vnstat # check vnstat
+
+curl -s https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/docker_install.sh | bash
+#sudo docker run hello-world # check docker
+
+curl -s https://raw.githubusercontent.com/russskiykorablidinah/pihpah/main/system_prepare.sh | bash
